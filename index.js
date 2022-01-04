1978b810d40f51837ddd2616eb514d767e377208
| Ali soleymani | <alisoleimani199@gmail.com>
2022-01-04 11:15:35 +0330

    Render App component

---
+++ index.js
@@ -0,0 +1,11 @@
+//! App component
+function App(props) {
+    return (
+        <>
+            Hello World!
+        </>
+    );
+}
+
+//! Render app
+ReactDOM.render(<App />, document.getElementById("root"))
