9f98c2b678de53692578ecaec762539395844ee8
| Ali soleymani | <alisoleimani199@gmail.com>
2022-01-04 13:33:38 +0330

    Use tailwind builder

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  content: ['./*.{html,js,htm}'],
+  theme: {
+    colors: {
+      "white": "#ffffff",
+      "light": "#dbdbdb",
+      "gray": "#828282",
+      "light-gray": "#C4C4C4",
+      "primary": "#080034",
+      "light-primary": "#040d5a",
+      "rank": "#278f04",
+    },
+    extend: {},
+  },
+  plugins: [],
+}
